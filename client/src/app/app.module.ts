052d2ffced1759a84356360b7d982cb3344bfb5a
Dhruv Ramdev <pythonbrilliant@gmail.com>
2017-12-03 06:10:00 +0530

    Build

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http' ;
-import {Ng2Webstorage} from 'ngx-webstorage';
 
 
 import { AppComponent } from './app.component';
